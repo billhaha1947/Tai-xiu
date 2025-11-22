25011149c93295cc382b2ea9eb6d806345f2fd6b
billhaha1947 <dangnhatminh18122008@gmail.com>
2025-11-22 18:50:23 +0700

    Update socket.js

--- frontend/assets/socket.js
+++ frontend/assets/socket.js
@@ -1,25 +1,13 @@
-// socket.js
-const socket = io(window.location.origin);
+// socket.js (ES module)
+const socket = io(window.location.origin, { transports:['websocket'] });
 
 socket.on('connect', ()=> console.log('socket connected'));
-socket.on('round_start', (data)=> {
-  console.log('round_start', data);
-  // set timer UI
-});
-socket.on('round_tick', (data)=> {
-  // update timer
-  document.getElementById('round-timer').innerText = data.left;
-});
-socket.on('round_end', (data)=> {
-  console.log('round_end', data);
-  // animate dice via three-dice import
-  import('./three-dice.js').then(mod=>{
-    const dice = data.round.dice.split(',').map(x=>parseInt(x));
-    mod.rollToResult(dice);
-  });
-  // update balances etc.
-});
-socket.on('chat_message', (m)=> {
-  // append to chat window
-});
+socket.on('disconnect', ()=> console.log('socket disconnected'));
+
+// forward important events to global handlers used in script.js
+socket.on('round_start', (d)=> window.dispatchEvent(new CustomEvent('sx_round_start', {detail:d})));
+socket.on('round_tick', (d)=> window.dispatchEvent(new CustomEvent('sx_round_tick', {detail:d})));
+socket.on('round_end', (d)=> window.dispatchEvent(new CustomEvent('sx_round_end', {detail:d})));
+socket.on('chat_message', (m)=> window.dispatchEvent(new CustomEvent('sx_chat_message', {detail:m})));
+
 export default socket;
