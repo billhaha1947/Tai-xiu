95fb04e750f7152dd11349684bb44935c9ae0591
billhaha1947 <dangnhatminh18122008@gmail.com>
2025-11-22 18:54:39 +0700

    Update socket.js

--- frontend/assets/socket.js
+++ frontend/assets/socket.js
@@ -1,10 +1,10 @@
 // socket.js (ES module)
 const socket = io(window.location.origin, { transports:['websocket'] });
 
-socket.on('connect', ()=> console.log('socket connected'));
-socket.on('disconnect', ()=> console.log('socket disconnected'));
+socket.on('connect', ()=> console.log('[socket] connected'));
+socket.on('disconnect', ()=> console.log('[socket] disconnected'));
 
-// forward important events to global handlers used in script.js
+// dispatch events for script.js
 socket.on('round_start', (d)=> window.dispatchEvent(new CustomEvent('sx_round_start', {detail:d})));
 socket.on('round_tick', (d)=> window.dispatchEvent(new CustomEvent('sx_round_tick', {detail:d})));
 socket.on('round_end', (d)=> window.dispatchEvent(new CustomEvent('sx_round_end', {detail:d})));
